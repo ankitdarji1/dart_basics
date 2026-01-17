9b9c3f6ef9a85b45a1a63313ad6c3bd606ce276d
Ankit Darji <ankit.darji@erda.com>
2026-01-17 10:07:53 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    // void main() {
+    //
+    //   String lord='Ganesh';
+    //
+    //   int presentstill=2026;
+    //
+    //   double comparewithus=3.7;
+    //
+    //   bool mission=false;
+    //
+    //   List<String> flyObjects=['Jupiter','Sarturn','Uranus','Neptune'];
+    //
+    //   Map<String,dynamic> image={
+    //     'tag':['Saturn'],
+    //     'url':'https://www.bing.com/ck/a?!&&p=e8de6abe8ec7ea846c40c20cc65633e2b2c1f5fc9966a1e4e027ba98253a97c5JmltdHM9MTc2ODUyMTYwMA&ptn=3&ver=2&hsh=4&fclid=1f1c38ac-e82c-6902-238f-2e60e9d4684b&u=a1L2ltYWdlcy9zZWFyY2g_cT1zYXR1cm4raW1hZ2VzJmlkPTdBQjkzMjRFNEY0Q0Y2OUUzRjEyQzYxNTZGNUUzNUUyMjBGNEJDNkMmRk9STT1JQUNGSVI&ntb=1',
+    //   };
+    //
+    //   String name='Abhishek';
+    //   double age=22.06;
+    //   var height=22.66;
+    //   bool  value=true;
+    //   List<double> ages=[22.20,10.11,11.21,12.26];
+    //   Map<String,dynamic> tags={
+    //     'hero':{'Amitabachan'},
+    //     'url':''
+    //   };
+    //
+    //
+    //   print('Lord: $lord');
+    //   print('Presend Now: $presentstill');
+    //   print('Compare With: $comparewithus');
+    //   print('Mission: $mission');
+    //   print('FlyObjects: $flyObjects');
+    //   print('image: $image');
+    //   print('Name: $name');
+    //   print('Age: $age');
+    //   print('Height:$height');
+    //   print('height: $height');
+    //   print('Mission: $value');
+    //   print('Ages: $ages');
+    //   print('tags: $tags');
+    //
+    //
+    // }
+    );
+  }
+}
