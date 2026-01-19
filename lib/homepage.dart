cf53ea9265b508e07e08b55a95897799712ce8f0
Ankit Darji <ankit.darji@erda.com>
2026-01-19 10:02:38 +0530

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -56,6 +56,124 @@ class _HomepageState extends State<Homepage> {
     //
     //
     // }
+
+      // void main(){
+
+// var name='Abhishek';
+// var year=1988;
+// var antennaDiameter=3.7;
+// var playername=['Sachine','Virat','Sehwag'];
+// var image={
+//     'tag':['people'],
+//     'url':''
+// };
+
+// print(name);
+// print(year);
+// print(antennaDiameter);
+// print(image);
+// }
+
+// void main(){
+
+// var name="Kunal";
+// var age=17;
+// var height=5.7;
+// var friends=['Tunal','Abhishek','Krusha'];
+// var tags={
+//     'tag':['people'],
+//     'url':'',
+// };
+
+// print('Name:$name');
+// print('Age:$age');
+// print('Height:$height');
+// print('Friends:$friends');
+// print('Tags:$tags');
+// }
+
+// void main(){
+//     var year=1977;
+
+//     if(year>=2001){
+//         print('21st century');
+//     }else if(year>=1901){
+//         print('20th century');
+//     }
+// }
+
+// void main(){
+//     var marks=75;
+
+//     if(marks>=90){
+//         print('Grade A');
+//     }else if(marks>=75){
+//         print('Grade B');
+//     }else if(marks>=65){
+//         print('Grade C');
+//     }else{
+//         print('Fails');
+//     }
+// }
+
+// void main(){
+//     var age=20;
+
+//     if(age>=18){
+//         print('You are eligible for votings');
+//     }else{
+//         print('You are not eligible for votings');
+//     }
+// }
+
+// void main(){
+//     var securitycode=21;
+
+//     if(securitycode==20){
+//         print('You are authoridse');
+//     }
+//     else{
+//         print('You are not Authoridse');
+//     }
+// }
+
+// void main(){
+//     for(int month=1;month<=12;month++){
+//         print(month);
+//     }
+// }
+
+// void main(){
+//     for(int i=1;i<=100;i++){
+//         print('Rollno:$i');
+//     }
+// }
+
+// void main(){
+//     for(var i=1;i<10;i++){
+//         print(i);
+//     }
+// }
+
+// void main(){
+//     var year=1977;
+//     while(year<2016){
+//         year+=1;
+//     }
+//     print(year);
+// }
+
+// void main(){
+//     var count=1;
+//     while(count<=5){
+//         print('Count:$count');
+//         count++;
+//     }
+// }
+
+
+
+
     );
   }
 }
